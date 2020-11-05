1fd9de5a21a9b5fe2996503cdcebb4c5cf0417c3
Carlos Martins <lemos_oliveiraa@hotmail.com>
2020-11-05 11:49:39 -0300

    Delete seller

--- SalesWeb/Services/SellerService.cs
+++ SalesWeb/Services/SellerService.cs
@@ -20,6 +20,18 @@ namespace SalesWeb.Services
             return _context.Sellers.ToList();
         }
 
+        public Seller FindById(int id)
+        {
+            return _context.Sellers.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Sellers.Find(id);
+            _context.Sellers.Remove(obj);
+            _context.SaveChanges();
+        }
+
         public void Insert(Seller obj)
         {
             _context.Add(obj);
